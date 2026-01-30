b07c5267b79483bbc8312a3ad20c8d54636bc6d9
Saurabh Yadav <yadavsaurabhrajepur@gmail.com>
2026-01-30 10:21:10 +0530

    adding suggestion box

--- script.js
+++ script.js
@@ -33,7 +33,62 @@ searchBtn.addEventListener("click", () => {
     const query = searchInput.value.trim();
     if(!query) return;
     // console.log("Searching for:", query);
+
+
+    // to store search history in local storage
+    let history = JSON.parse(localStorage.getItem("searchHistory")) || [];
+    if(!history.includes(query)){
+        history.push({
+        query: query,
+        time:Date.now()
+    });
+    localStorage.setItem("searchHistory", JSON.stringify(history));
+    }
+    // ................
     
     window.location.href =`search.html?q=${encodeURIComponent(query)}`; 
     searchInput.value = "";
 });
+
+
+// searchBtn.addEventListener("click", () => {
+
+//     let history = JSON.parse(localStorage.getItem("searchHistory")) || [];
+//     if(!history.includes(query)){
+//         history.push({
+//         query: query,
+//         time:Date.now()
+//     });
+//     localStorage.setItem("searchHistory", JSON.stringify(history));
+//     }
+
+//     window.location.href =`search.html?q=${encodeURIComponent(query)}`; 
+//     searchInput.value = "";
+// });
+
+
+const suggestionBox = document.getElementById("suggestions");
+
+searchInput.addEventListener("input", () => {
+    // console.log("Suggeestion working");
+    const text= searchInput.value.toLowerCase();
+    const history = JSON.parse(localStorage.getItem("searchHistory")) || [];
+    const matches = history.filter(item => item.query.toLowerCase().includes(text));
+    // console.log(history);
+    suggestionBox.innerHTML = "";
+
+    console.log(matches);
+    matches.forEach(item => {
+        const div = document.createElement("div");
+        div.className = "suggestion-item";
+        div.innertext = item.query;
+
+        div.addEventListener("click", () => {
+            searchInput.value = item.query;
+            suggestionBox.innerHTML = "";
+        });
+
+        suggestionBox.appendChild(div);
+    });
+    });
+
